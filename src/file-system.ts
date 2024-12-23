69c769c796f073f57c7f98b445bdd3abfba53ac5
Lucas <lucasnrgaard@gmail.com>
2024-12-23 07:09:13 +0100

    chore: lint

--- src/file-system.ts
+++ src/file-system.ts
@@ -1,7 +1,7 @@
 import type { DirectoryJSON } from "./types";
 import { readdirSync, readFileSync, readlinkSync, statSync } from "node:fs";
 import { readdir, readFile, readlink, stat } from "node:fs/promises";
-import { normalize, sep as pathSeparator } from "node:path";
+import { normalize } from "node:path";
 import { FIXTURE_ORIGINAL_PATH } from "./constants";
 import { symlink } from "./utils";
 
