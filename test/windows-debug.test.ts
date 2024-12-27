ef85b078e49aa265009907b8c59fc739387e4c86
Lucas <lucasnrgaard@gmail.com>
2024-12-27 06:38:03 +0100

    chore: debug windows

--- test/windows-debug.test.ts
+++ test/windows-debug.test.ts
@@ -18,26 +18,14 @@ it.runIf(platform() === "win32")("should be able to create files with different 
   createFileTreeSync("./.vitest-testdirs/windows-test", files);
 
   const file1Stats = await stat("./.vitest-testdirs/windows-test/file1.txt");
-  console.error({
-    mode: (file1Stats.mode & 0o644).toString(8),
-    stats: JSON.stringify(file1Stats, null, 2),
-  });
+  console.error("file1", file1Stats);
 
   const dir1Stats = await stat("./.vitest-testdirs/windows-test/dir1");
-  console.error({
-    mode: (dir1Stats.mode & 0o777).toString(8),
-    stats: JSON.stringify(dir1Stats, null, 2),
-  });
+  console.error("dir1", dir1Stats);
 
   const file2Stats = await stat("./.vitest-testdirs/windows-test/dir1/file2.txt");
-  console.error({
-    mode: (file2Stats.mode & 0o444).toString(8),
-    stats: JSON.stringify(file2Stats, null, 2),
-  });
+  console.error("file2", file2Stats);
 
   const dir2Stats = await stat("./.vitest-testdirs/windows-test/dir1/dir2");
-  console.error({
-    mode: (dir2Stats.mode & 0o777).toString(8),
-    stats: JSON.stringify(dir2Stats, null, 2),
-  });
+  console.error("dir2", dir2Stats);
 });
