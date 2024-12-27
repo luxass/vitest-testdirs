1469ecb4d9fa265e3ef5435330179508cc2678a5
Lucas <lucasnrgaard@gmail.com>
2024-12-27 07:55:18 +0100

    chore: debug windows

--- test/test-windows.test.ts
+++ test/test-windows.test.ts
@@ -9,19 +9,19 @@ it.runIf(platform() === "win32")("windows", async () => {
     "dir1": {
       "file2.txt": withMetadata("Hello, World!", { mode: 0o444 }),
       "dir2": withMetadata({
-        "file3.txt": "This is file 3",
+        "file3.txt": "Hello, World!",
       }, { mode: 0o555 }),
     },
   });
 
   try {
-    await writeFile(`${path}/dir1/file2.txt`, "Hello, Vitest!");
+    await writeFile(`${path}/dir1/dir2/file3.txt`, "Hello, Vitest!");
     // This should throw an error, but not on Windows
   } catch (err) {
     console.error(err);
   }
 
-  const content = await readFile(`${path}/dir1/file2.txt`, "utf8");
+  const content = await readFile(`${path}/dir1/dir2/file3.txt`, "utf8");
   // The content is now changes, but it should not be possible to write to the file
 
   expect(content).not.toBe("Hello, Vitest!");
